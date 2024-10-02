a02ecbffafa479ce00667717f8f8d4c86715daad
noura <noura.tu19@gmail.com>
2024-10-02 14:23:32 +0300

    update

--- src/App.jsx
+++ src/App.jsx
@@ -4,13 +4,15 @@ import Products from './components/Products';
 function App() {
   return (
     <div className="flex flex-col sm:flex-row   w-full  ">
-     
+        <div className='flex justify-between'>
+        <h1 className="     p-5 bg-blue-800 text-white">   lab1 </h1>
+        </div>
       <div className='sm:w-1/2'>
       <h1 className='text-3xl font-bold mb-4'>Products</h1>
       <Products />
       </div>
     <div className='flex justify-between'>
-    <h1 className="     p-5 bg-blue-800 text-white">   lab1 </h1>
+  
     <h1 className="     p-5 bg-blue-800 text-white">   lab2  </h1>
     </div>
  
